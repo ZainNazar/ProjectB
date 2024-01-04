c381182018d04aca23dbb6b71c27056480d9d847
zalkh <zalkh@10.0.0.55>
2024-01-03 20:30:25 -0600

    Implemented add()

--- src/b/DemoB.java
+++ src/b/DemoB.java
@@ -3,7 +3,13 @@ package b;
 public class DemoB {
 
 	public static void main(String[] args) {
-System.out.println("Tuesday");
+		System.out.println("Tuesday");
+	}
+
+	
+	// Sprint 2 automation
+	public static void add(int a, int b) {
+		System.out.println(a + b);
 	}
 
 }
